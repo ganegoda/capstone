f72ce3e4b51bf8f83860e208b3b138967d6a4b78
Hasitha <hasitha1@gmail.com>
2022-03-20 00:24:31 -0500

    formated table_queries.py

--- table_queries.py
+++ table_queries.py
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS public.immigration_stg (
 )
 DISTSTYLE EVEN
 """
-#staging
+# staging
 create_us_city_demo_staging = """
 CREATE TABLE IF NOT EXISTS public.us_city_demo_stg (
     city VARCHAR(255),
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS public.us_city_demo_stg (
 )
 DISTSTYLE AUTO
 """
-#staging
+# staging
 create_airport_codes_staging = """
 CREATE TABLE IF NOT EXISTS public.airport_codes_stg (
     ident VARCHAR(10),
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS public.airport_codes_stg (
 )
 DISTSTYLE AUTO
 """
-#staging
+# staging
 create_world_temp_staging = """
 CREATE TABLE IF NOT EXISTS public.world_temp_stg (
     dt DATE SORTKEY DISTKEY,
@@ -83,7 +83,7 @@ DISTSTYLE KEY
 """
 
 
-#----------------dim from SAS file----------------------------------
+# ----------------dim from SAS file----------------------------------
 create_country = """
 CREATE TABLE IF NOT EXISTS country (
     country_code INTEGER PRIMARY KEY,
@@ -126,7 +126,7 @@ CREATE TABLE IF NOT EXISTS visa_type (
 DISTSTYLE ALL;
 """
 
-#----------------dim from manually collected data CSVs-------------------
+# ----------------dim from manually collected data CSVs-------------------
 create_gender_type = """
 CREATE TABLE IF NOT EXISTS gender (
     gender_code VARCHAR(1) PRIMARY KEY,
@@ -168,7 +168,7 @@ CREATE TABLE IF NOT EXISTS immigration (
     update_flag CHAR(1),
     match_flag CHAR(1),
     birth_year INTEGER,
-    admission_date VARCHAR(20),
+    admission_date DATE,
     gender CHAR(1) REFERENCES gender(gender_code),
     ins_num VARCHAR(20),
     airline VARCHAR(20),
@@ -180,23 +180,27 @@ DISTSTYLE KEY
 """
 
 
-
-
-
-table_list = ['immigration','immigration_stg', 'us_city_demo_stg', 'airport_codes_stg', 'world_temp_stg',
-               'country', 'entry_port', 'arrival_mode', 'region', 'visa_type',
+table_list = ['immigration', 'immigration_stg', 'us_city_demo_stg', 'airport_codes_stg', 'world_temp_stg',
+              'country', 'entry_port', 'arrival_mode', 'region', 'visa_type',
               'gender', 'visa']
 
 drop_table_template = "DROP TABLE IF EXISTS public.{}"
-truncate_table_template ="TRUNCATE TABLE IF EXISTS public.{}"
+truncate_table_template = "TRUNCATE TABLE IF EXISTS public.{}"
 
 
 # delete, create, turncate all tables
-drop_table_queries = [drop_table_template.format(table) for table in table_list]
+drop_table_queries = [drop_table_template.format(
+    table) for table in table_list]
 
-create_table_queries = [create_table_immigration_staging, create_us_city_demo_staging,create_airport_codes_staging,create_world_temp_staging,
- create_country, create_entry_port, create_arrival_mode, create_region,
-create_visa_type, create_gender_type, create_visa_class,create_table_immigration
-]
+create_table_queries = [create_table_immigration_staging, 
+                        create_us_city_demo_staging, 
+                        create_airport_codes_staging, 
+                        create_world_temp_staging,
+                        create_country, create_entry_port, 
+                        create_arrival_mode, create_region,
+                        create_visa_type, create_gender_type, 
+                        create_visa_class, create_table_immigration
+                        ]
 
-truncate_table_queries = [truncate_table_template.format(table) for table in table_list]
+truncate_table_queries = [
+    truncate_table_template.format(table) for table in table_list]
